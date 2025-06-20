b9d81f7f8f447ecb67aad12ab0c6f4549f4d64e1
Ricardo Gore <rjhgore@gmail.com>
2025-06-20 15:04:48 -0700

    Update curate_nucleotide_conoServerDB.R

--- Rproject/curate_nucleotide_conoServerDB.R
+++ Rproject/curate_nucleotide_conoServerDB.R
@@ -9,7 +9,6 @@
 # For every taxon, obtain Worms taxonomy lineage for further purposes (Fernando)
 
 
-
 rm(list = ls())
 
 if(!is.null(dev.list())) dev.off()
@@ -17,9 +16,12 @@ if(!is.null(dev.list())) dev.off()
 options(stringsAsFactors = FALSE, readr.show_col_types = FALSE)
 
 
-# url <- 'https://www.conoserver.org/download/conoserver_structure.xml.gz'
+# Nucleotide
+
+url <- 'https://www.conoserver.org/download/conoserver_nucleic.xml.gz'
+
 # Open a connection to the gzipped file at the URL
-# con <- gzcon(url(url, "rb"))
+con <- gzcon(url(url, "rb"))
 
 
 # Install required packages if needed
@@ -28,27 +30,120 @@ options(stringsAsFactors = FALSE, readr.show_col_types = FALSE)
 
 library(xml2)
 
-dir <- "~/"
+library(tidyverse)
 
-f <- ""
+# if as file:
 
-doc <- read_xml(con,encoding = "")
+outdir <- "~/Documents/GitHub/ConotoxinBenchmark/INPUTS/"
 
-doc <- read_html(con)
+# f <- list.files(path = dir, pattern = "xml.gz", full.names = T)
+# Open a gzipped connection and read the lines
 
-# Read the XML content as text
-xml_content <- readLines(con, warn = FALSE)
+# con <- gzfile(f[2], open = "rt") # 'rt' for reading text
+
+xml_lines <- readLines(con, warn = FALSE)
 
 close(con)
 
-# Parse the XML content
-doc <- read_xml(paste(xml_content, collapse = "\n"))
+# Collapse lines and parse as XML
+# (Read as html to support special character as greek letters)
 
-# Extract all 'geneSuperfamily' nodes
-geneSuperfamily_nodes <- xml_find_all(doc, ".//geneSuperfamily")
+doc <- read_html(paste(xml_lines, collapse = "\n")) # options = c("NOBLANKS", "NSCLEAN")
 
-# Extract their text values into a vector
-geneSuperfamily_vec <- xml_text(geneSuperfamily_nodes)
 
-# Print the vector
-print(geneSuperfamily_vec)
+all_nodes <- xml_find_all(doc, "//*",)
+
+# Extract all node names
+node_names <- xml_name(all_nodes)
+
+# Count occurrences of each node name
+table(node_names)
+
+# Extract fields features for nucleotide level
+# must match 3073 records nrow(Nodedf %>% drop_na(id))
+
+xpath_vec_nuc <- c(
+  "proteinid", "proteinname","genesuperfamily","proteintype","proteinsequence",
+  "organismlatin","organismdiet","organismregion","id","name","sequence")
+
+result <- sapply(xpath_vec_nuc, function(xpath) { xml_text(xml_find_first(all_nodes, xpath)) })
+
+result <- as.data.frame(result, stringsAsFactors = FALSE) %>% as_tibble()
+
+colnames(result) <- xpath_vec_nuc # xpath_vec_prot
+
+# Creates a fasta file and input
+# Filtering criteria
+# choosing only nucleotides of precursor type and proteinsequence
+
+Nodedf <- result %>% drop_na(id) 
+
+Nodedf %>% 
+  count(organismlatin) %>% filter(n > 10) %>% view()
+
+Nodedf <- Nodedf %>% 
+  count(sequence, id, organismlatin, name, sort = T) %>%
+  mutate(sequence = str_to_upper(sequence))
+
+Nodedf %>% ggplot(aes(nchar(sequence))) + geom_histogram()
+
+# Write fasta using a apply to write 
+
+Nodedf <- Nodedf %>% mutate(split_as = organismlatin)
+
+v <- Nodedf %>% count(split_as, sort = T) %>% pull(split_as)
+
+v <- tail(v)
+
+# For each group, subset, format, and write FASTA
+for (group in v) {
+  # Filter for the current group
+  tmp <- Nodedf %>%
+    filter(split_as == group) %>%
+    unite("id", id:name, sep = "|") %>%
+    pull(sequence, name = id)
+  
+  # Make DNAStringSet
+  seqs <- Biostrings::DNAStringSet(tmp)
+  
+  # Write to FASTA, use group name in file
+  fasta_file <- file.path(outdir, paste0("conopeptides_", group, ".fasta"))
+  Biostrings::writeXStringSet(seqs, fasta_file)
+}
+
+seqs <- Nodedf %>% 
+  unite("id", id:name, sep = "|") %>%
+  pull(sequence, name = id)
+
+seqs <- Biostrings::DNAStringSet(seqs)
+
+
+Biostrings::writeXStringSet(seqs, file.path(pub_dir, "conopeptides.fasta"))
+
+# 
+# gene2GO <- split(strsplit(gene2GO$GOs, ";") , gene2GO$gene_id)
+# gene2GO <- lapply(gene2GO, unlist)
+
+
+# PRotein =====
+# url <- 'https://www.conoserver.org/download/conoserver_protein.xml.gz'
+# head(xml_lines[grepl("nucleicAcidId", xml_lines) ], n = 100)
+
+# N00316N00540N02519N02520
+# xml_lines[grepl("N00940|N00316|N00540|N02519|N02520", xml_lines) ]
+
+# head(xml_lines[grepl("N00316|", xml_lines) ], n = 100)
+
+# OR Extract features for protein level
+
+# conoserver identifier|name|organism|protein type|toxin class|gene superfamily|cysteine framework|pharmacological
+# must match 8523 records nrow(Nodedf %>% drop_na(id))
+
+xpath_vec_prot <- c(
+  "class", "cysteineframewrok","genesuperfamily","pharmacologicalfamily",
+  "nucleicacid", "nucleicacidid",
+  "organismlatin","organismdiet","organismregion","id","name","sequence")
+
+
+
+
