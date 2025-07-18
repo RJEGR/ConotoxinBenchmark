602fcbb78babaa271a072cd335ac36f7208224c9
Ricardo Gore <rjhgore@gmail.com>
2025-07-18 16:53:50 -0700

    Update Run_bridger.sh

--- run_assemblers_dir/Run_bridger.sh
+++ run_assemblers_dir/Run_bridger.sh
@@ -11,7 +11,8 @@ CPU=$4
 export PATH=/LUSTRE/bioinformatica_data/genomica_funcional/rgomez/Software/Bridger_r2014-12-01:$PATH
 
 which Bridger.pl
-#--output/-o
+
+rm -rf $OUTDIR
 
 call="Bridger.pl --seqType fq --left $forward_fq --right $reverse_fq --output $OUTDIR --CPU $CPU"
 
@@ -19,7 +20,7 @@ echo $call
 
 eval $call
 
-BS=`echo $OUTDIR | awk -F'_' '{print $1"_"$2}'`
+BS=`echo $(basename $OUTDIR) | awk -F'_' '{print $1"_"$2}'`
 
 # mv $OUTDIR/Bridger.fasta ${BS}_FASTA_DIR/${OUTDIR%_dir}.fa
 movecall="mv $OUTDIR/Bridger.fasta ${BS}_FASTA_DIR/${OUTDIR%_dir}.fa"
