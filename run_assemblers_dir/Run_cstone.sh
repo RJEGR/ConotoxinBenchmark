95eece8c380491e48b70c97d8bd38b9b2a550129
Ricardo Gore <rjhgore@gmail.com>
2025-07-18 16:53:52 -0700

    Update Run_cstone.sh

--- run_assemblers_dir/Run_cstone.sh
+++ run_assemblers_dir/Run_cstone.sh
@@ -10,18 +10,19 @@ MEM=$5
 
 
 #export PATH=/LUSTRE/bioinformatica_data/genomica_funcional/rgomez/Software/cstone:$PATH
+rm -rf $OUTDIR
 
 WD=/LUSTRE/bioinformatica_data/genomica_funcional/rgomez/Software/cstone
 cstone="java -jar -Xmx64G $WD/cstone.jar"
 
 
-call="$cstone -r1 $forward_fq -r2 $reverse_fq -p $CPU  -o $OUTDIR"
+call="$cstone -r1 $forward_fq -r2 $reverse_fq -o $OUTDIR -p $CPU"
 
 echo $call
 
 eval $call
 
-BS=`echo $OUTDIR | awk -F'_' '{print $1"_"$2}'
+BS=`echo $(basename $OUTDIR) | awk -F'_' '{print $1"_"$2}'`
 
 movecall="mv $OUTDIR/contigs.fasta ${BS}_FASTA_DIR/${OUTDIR%_dir}.fa"
 echo $movecall
