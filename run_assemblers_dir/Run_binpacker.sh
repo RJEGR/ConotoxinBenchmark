b2b0af035d209161c87f370ac1c671fcd7e719d2
Ricardo Gore <rjhgore@gmail.com>
2025-07-18 16:53:48 -0700

    Update Run_binpacker.sh

--- run_assemblers_dir/Run_binpacker.sh
+++ run_assemblers_dir/Run_binpacker.sh
@@ -12,13 +12,15 @@ export PATH=/LUSTRE/bioinformatica_data/genomica_funcional/rgomez/Software/BinPa
 
 which BinPacker
 
+rm -rf $OUTDIR
+
 call="BinPacker -s fq -p pair -l $forward_fq -r $reverse_fq -o $OUTDIR"
 
 echo $call
 
 eval $call
 
-BS=`echo $OUTDIR | awk -F'_' '{print $1"_"$2}'`
+BS=`echo $(basename $OUTDIR) | awk -F'_' '{print $1"_"$2}'`
 
 movecall="mv $OUTDIR/BinPacker_Out_Dir/BinPacker.fa ${BS}_FASTA_DIR/${OUTDIR%_dir}.fa"
 echo $movecall
