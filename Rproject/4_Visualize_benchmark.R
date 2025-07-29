47fd68036c21b3acaea8d56a1118d5c8661cb7b8
Ricardo Gore <rjhgore@gmail.com>
2025-07-29 15:31:49 -0700

    Create 4_Visualize_benchmark.R

---
+++ Rproject/4_Visualize_benchmark.R
@@ -0,0 +1,9 @@
+
+# Some EDA for answere null H
+# 1. The organismdiet corrrelates with sequence complexity (gene superfamily)?
+# 2. Sequence complexity is related to assembly accuracy?
+# 3. structurally heterogeneous domain architectures (SHDA) is related with sequence complexity? 
+# 4. SHDA correlates with assembly accuracy?
+
+
+# Results from 
