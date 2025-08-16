2c495788a624895b5274a14354672e9e871114ba
Ricardo Gore <rjhgore@gmail.com>
2025-08-16 00:41:47 -0700

    Update Accuracy.sh

--- Accuracy.sh
+++ Accuracy.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-# write help arguments to include REFDIR=/LUSTRE/bioinformatica_data/genomica_funcional/rgomez/fernando_pub/reads_artificiales/inputs
-
+# 
 
 while getopts "s:r:d:h" opt; do
     case $opt in
         s) MANIFEST_FILE="$OPTARG";;
-        r) REFDIR="$OPTARG";;
-        d) FASTA_DIR="$OPTARG";;
+        r) TARGET="$OPTARG";;
+        d) QUERY="$OPTARG";;
         h)
-            echo "Usage: $(basename $0) -s <Manifest> [-r <Reference directory>] [-d <sequences Directory>]"
+            echo "Usage: $(basename $0) -s <Manifest> [-d <query sequences>] [-r <Target database>]"
             echo
             echo "Arguments:"
-            echo "  -d <Fasta directory> Relative or absolute path to the directory containing fasta files."
             echo "  -s <Manifest>: A text file containing sample information. Option -s all, allow to run assembly batches for all manifests matching the pattern *.txt."
-            echo "  -r <Reference>: Relative or absolute path to the directory containing fasta files."
+            echo "  -d <Query sequences> database and query are each either a fasta format"
+            echo "  -r <Target database>: Database and query are each either a fasta format."
 
             echo
             echo "Description:"
@@ -32,74 +31,150 @@ done
 shift $((OPTIND-1))
 
 
-if [[ -z "$FASTA_DIR" || -z "$MANIFEST_FILE" || -z "$REFDIR" ]]; then
-    echo "Error: Missing required arguments."
+if { [[ -z "$QUERY" || -z "$MANIFEST_FILE" || -z "$TARGET" ]] &&  [[ -z "$QUERY" || -z "$TARGET" ]] && [[ -z "$QUERY" || -z "$MANIFEST_FILE" ]]; }; then
+    echo "Error: You must provide either QUERY and TARGET, or QUERY and MANIFEST_FILE."
     echo "Run '$(basename $0) -h' for usage information."
     exit 1
 fi
 
 
-export PATH=/LUSTRE/apps/bioinformatica/.local/bin:$PATH
-export PATH=/LUSTRE/apps/bioinformatica/ruby2/ruby-2.2.0/bin:$PATH
-export LD_LIBRARY_PATH=/LUSTRE/apps/bioinformatica/ruby2/ruby-2.2.0/lib:$LD_LIBRARY_PATH
+run_longer_transrate() {
 
-which transrate
+    export PATH=/LUSTRE/apps/bioinformatica/.local/bin:$PATH
+    export PATH=/LUSTRE/apps/bioinformatica/ruby2/ruby-2.2.0/bin:$PATH
+    export LD_LIBRARY_PATH=/LUSTRE/apps/bioinformatica/ruby2/ruby-2.2.0/lib:$LD_LIBRARY_PATH
 
-run_transrate() {
-    
-    local REFDIR="$1"
+    #which transrate
 
-    local FASTA_DIR="$2"
-
-    local MANIFEST_FILE="$3"
 
     mkdir -p transrate_tmp_dir
+    mkdir -p transrate_contigs_dir
     
-    find $FASTA_DIR -maxdepth 1 -type f -name "*.fa" | while read -r asm; do
-        
-        echo "Processing FASTA : $asm"
+    local TARGET="$1"
 
-        # Substring to extract the sf prefix
+    local QUERY="$2"
 
-        FAMILYPREFIX=$(echo $asm | awk -F'/' '{for(i=1;i<=NF;i++) if($i ~ /_FASTA_DIR/) print $i}')
+    local Manifest="$3"
 
 
-        echo ${FAMILYPREFIX%_FASTA_DIR}
-
-        ref=$REFDIR/${FAMILYPREFIX%_FASTA_DIR}.fasta
-        
-        
-        TRANSRATE_DIR=${asm%.fa}_dir
-
-        call="transrate --assembly $asm --reference $ref --output transrate_tmp_dir/$TRANSRATE_DIR --threads 20"
-
-        echo "Executing: $call"
-
-        #eval $call
-
-        echo "Finished processing FASTA : $asm"
+    local forward_fq=${Manifest%.*}_concat_PE1.fq
     
-    done
+    local reverse_fq=${Manifest%.*}_concat_PE2.fq
 
+    #BS=$(awk '{print $1}' "$Manifest")
 
+    BS=${Manifest%.*}
+
+    local call=$(awk '{print $2}' "$Manifest" | tr "\n" " ")
+    for fq in $call; do [[ -n "$fq" ]] && echo "$fq"; done | xargs cat > "$forward_fq"
+
+    local call=$(awk '{print $3}' "$Manifest" | tr "\n" " ")
+    for fq in $call; do [[ -n "$fq" ]] && echo "$fq"; done | xargs cat > "$reverse_fq"
+
+    TRANSRATE_DIR=${BS}_transrate_dir
+    
+    TRANSRATE_DIR=${QUERY%.*}_dir
+
+    call="transrate --left $forward_fq --right $reverse_fq --assembly $QUERY --reference $TARGET --output transrate_tmp_dir/$TRANSRATE_DIR --threads 20"
+
+    echo "Executing: $call"
+
+    eval $call
+
+    rm "$forward_fq"
+    rm "$reverse_fq"
+
+    echo "Finished processing FASTA in transrate_tmp_dir/$TRANSRATE_DIR"
+
+    echo "Moving results to final directory: transrate_contigs_dir"
+
+    find "transrate_tmp_dir/$TRANSRATE_DIR" -name "contigs.csv" | while read -r contig_file; do
+
+            dst="transrate_contigs_dir/${contig_file#transrate_tmp_dir}"
+            
+            echo $dst
+            
+            mkdir -p "$(dirname "$dst")"
+            
+            echo "Moving $contig_file to $dst"
+
+            mv "$contig_file" "$dst"
+        done
 
 }
 
-run_transrate "$REFDIR" "$FASTA_DIR" "$MANIFEST_FILE"
+run_fast_transrate() {
 
-exit
 
-mkdir -p transrate_contigs_dir
+    export PATH=/LUSTRE/apps/bioinformatica/.local/bin:$PATH
+    export PATH=/LUSTRE/apps/bioinformatica/ruby2/ruby-2.2.0/bin:$PATH
+    export LD_LIBRARY_PATH=/LUSTRE/apps/bioinformatica/ruby2/ruby-2.2.0/lib:$LD_LIBRARY_PATH
 
-find transrate_tmp_dir -name 'contigs.csv' -exec bash -c '
-        for src; do
-              dst="transrate_contigs_dir/${src#transrate_tmp_dir}"
-              echo $dst
-              mkdir -p "$(dirname "$dst")"
-              cp "$src" "$dst"
+    #which transrate
 
+
+    mkdir -p transrate_tmp_dir
+    mkdir -p transrate_contigs_dir
+    
+    local TARGET="$1"
+
+    local QUERY="$2"
+
+
+    
+    # find $QUERY -maxdepth 1 -type f -name "*.fa" | while read -r asm; do
+        
+    echo "Processing FASTA : $QUERY"
+
+        # Substring to extract the sf prefix
+
+        # FAMILYPREFIX=$(echo $asm | awk -F'/' '{for(i=1;i<=NF;i++) if($i ~ /_QUERY/) print $i}')
+
+
+        #echo ${FAMILYPREFIX%_QUERY}
+
+        #ref=$TARGET/${FAMILYPREFIX%_QUERY}.fasta
+        
+        
+    TRANSRATE_DIR=${QUERY%.*}_dir
+
+    echo "Transrate directory is : $TRANSRATE_DIR"
+
+    call="transrate --assembly $QUERY --reference $TARGET --output transrate_tmp_dir/$TRANSRATE_DIR --threads 20"
+
+    echo "Executing: $call"
+
+    eval $call
+
+    echo "Finished processing FASTA in transrate_tmp_dir/$TRANSRATE_DIR"
+
+    echo "Moving results to final directory: transrate_contigs_dir"
+
+    find "transrate_tmp_dir/$TRANSRATE_DIR" -name "contigs.csv" | while read -r contig_file; do
+
+            dst="transrate_contigs_dir/${contig_file#transrate_tmp_dir}"
+            
+            echo $dst
+            
+            mkdir -p "$(dirname "$dst")"
+            
+            echo "Moving $contig_file to $dst"
+
+            mv "$contig_file" "$dst"
         done
-' bash {} +
+    
+
+}
+
+
+if [[ -z "$MANIFEST_FILE" ]]; then
+    echo "Using "$TARGET" and "$QUERY" to calculate the full-length transcript coverage."
+    run_fast_transrate "$TARGET" "$QUERY"
+else
+    echo "Using "$TARGET" and "$QUERY" and $MANIFEST_FILE to calculate the full-length transcript coverage"
+    # 
+    run_longer_transrate "$TARGET" "$QUERY" "$MANIFEST_FILE"
+fi
 
 # rm -rf transrate_tmp_dir
 
