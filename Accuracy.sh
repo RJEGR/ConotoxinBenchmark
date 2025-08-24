2ad5584b77098bed619224e1c4ffc988a65595c2
Ricardo Gore <rjhgore@gmail.com>
2025-08-23 23:50:16 -0700

    Update Accuracy.sh

--- Accuracy.sh
+++ Accuracy.sh
@@ -2,18 +2,18 @@
 
 # 
 
-while getopts "s:r:d:h" opt; do
+while getopts "s:d:m:h" opt; do
     case $opt in
         s) MANIFEST_FILE="$OPTARG";;
-        r) TARGET="$OPTARG";;
+        m) MODE="$OPTARG";;
         d) QUERY="$OPTARG";;
         h)
-            echo "Usage: $(basename $0) -s <Manifest> [-d <query sequences>] [-r <Target database>]"
+            echo "Usage: $(basename $0) -s <Manifest> [-d <query sequences>]"
             echo
             echo "Arguments:"
-            echo "  -s <Manifest>: A text file containing sample information. Option -s all, allow to run assembly batches for all manifests matching the pattern *.txt."
+            echo "  -s <Manifest>: A text file containing sample information. Option < -s all >, allow to run assembly batches for all manifests matching the pattern *.txt."
             echo "  -d <Query sequences> database and query are each either a fasta format"
-            echo "  -r <Target database>: Database and query are each either a fasta format."
+            echo "  -m <Mode>: Assess reference-based < -m reference > or read-based < -m reads > metrics. If not provided, defaults to < -m both >."
 
             echo
             echo "Description:"
@@ -31,8 +31,8 @@ done
 shift $((OPTIND-1))
 
 
-if { [[ -z "$QUERY" || -z "$MANIFEST_FILE" || -z "$TARGET" ]] &&  [[ -z "$QUERY" || -z "$TARGET" ]] && [[ -z "$QUERY" || -z "$MANIFEST_FILE" ]]; }; then
-    echo "Error: You must provide either QUERY and TARGET, or QUERY and MANIFEST_FILE."
+if [[ -z "$QUERY" || -z "$MANIFEST_FILE" ]] ; then
+    echo "Error: You must provide both QUERY and MANIFEST_FILE."
     echo "Run '$(basename $0) -h' for usage information."
     exit 1
 fi
@@ -50,11 +50,11 @@ run_longer_transrate() {
     mkdir -p transrate_tmp_dir
     mkdir -p transrate_contigs_dir
     
-    local TARGET="$1"
+    #local TARGET="$1"
 
-    local QUERY="$2"
+    local QUERY="$1"
 
-    local Manifest="$3"
+    local Manifest="$2"
 
 
     local forward_fq=${Manifest%.*}_concat_PE1.fq
@@ -63,17 +63,19 @@ run_longer_transrate() {
 
     #BS=$(awk '{print $1}' "$Manifest")
 
-    BS=${Manifest%.*}
+    #BS=${Manifest%.*}
+
+    BS=${Manifest%.*}_${QUERY%.*}
 
     local call=$(awk '{print $2}' "$Manifest" | tr "\n" " ")
     for fq in $call; do [[ -n "$fq" ]] && echo "$fq"; done | xargs cat > "$forward_fq"
 
     local call=$(awk '{print $3}' "$Manifest" | tr "\n" " ")
     for fq in $call; do [[ -n "$fq" ]] && echo "$fq"; done | xargs cat > "$reverse_fq"
+    
+    local TARGET=$(awk '{print $4}' "$Manifest")
 
     TRANSRATE_DIR=${BS}_transrate_dir
-    
-    TRANSRATE_DIR=${QUERY%.*}_dir
 
     call="transrate --left $forward_fq --right $reverse_fq --assembly $QUERY --reference $TARGET --output transrate_tmp_dir/$TRANSRATE_DIR --threads 20"
 
@@ -103,7 +105,7 @@ run_longer_transrate() {
 
 }
 
-run_fast_transrate() {
+run_reference_transrate() {
 
 
     export PATH=/LUSTRE/apps/bioinformatica/.local/bin:$PATH
@@ -116,13 +118,13 @@ run_fast_transrate() {
     mkdir -p transrate_tmp_dir
     mkdir -p transrate_contigs_dir
     
-    local TARGET="$1"
-
-    local QUERY="$2"
-
-
+    local QUERY="$1"
     
-    # find $QUERY -maxdepth 1 -type f -name "*.fa" | while read -r asm; do
+    #local Manifest="$2"
+
+    local TARGET=$(awk '{print $4}' "$2")
+
+
         
     echo "Processing FASTA : $QUERY"
 
@@ -166,16 +168,83 @@ run_fast_transrate() {
 
 }
 
+run_read_transrate() {
 
-if [[ -z "$MANIFEST_FILE" ]]; then
-    echo "Using "$TARGET" and "$QUERY" to calculate the full-length transcript coverage."
-    run_fast_transrate "$TARGET" "$QUERY"
+    export PATH=/LUSTRE/apps/bioinformatica/.local/bin:$PATH
+    export PATH=/LUSTRE/apps/bioinformatica/ruby2/ruby-2.2.0/bin:$PATH
+    export LD_LIBRARY_PATH=/LUSTRE/apps/bioinformatica/ruby2/ruby-2.2.0/lib:$LD_LIBRARY_PATH
+
+    #which transrate
+
+
+    mkdir -p transrate_tmp_dir
+    mkdir -p transrate_contigs_dir
+    
+
+    local QUERY="$1"
+    local Manifest="$2"
+
+
+    local forward_fq=${Manifest%.*}_concat_PE1.fq
+    
+    local reverse_fq=${Manifest%.*}_concat_PE2.fq
+
+    BS=${Manifest%.*}_${QUERY%.*}
+
+    local call=$(awk '{print $2}' "$Manifest" | tr "\n" " ")
+    for fq in $call; do [[ -n "$fq" ]] && echo "$fq"; done | xargs cat > "$forward_fq"
+
+    local call=$(awk '{print $3}' "$Manifest" | tr "\n" " ")
+    for fq in $call; do [[ -n "$fq" ]] && echo "$fq"; done | xargs cat > "$reverse_fq"
+
+    TRANSRATE_DIR=${BS}_transrate_dir
+    
+    call="transrate --left $forward_fq --right $reverse_fq --assembly $QUERY  --output transrate_tmp_dir/$TRANSRATE_DIR --threads 20"
+
+    echo "Executing: $call"
+
+    eval $call
+
+    rm "$forward_fq"
+    rm "$reverse_fq"
+
+    echo "Finished processing FASTA in transrate_tmp_dir/$TRANSRATE_DIR"
+
+    echo "Moving results to final directory: transrate_contigs_dir"
+
+    find "transrate_tmp_dir/$TRANSRATE_DIR" -name "contigs.csv" | while read -r contig_file; do
+
+            dst="transrate_contigs_dir/${contig_file#transrate_tmp_dir}"
+            
+            echo $dst
+            
+            mkdir -p "$(dirname "$dst")"
+            
+            echo "Moving $contig_file to $dst"
+
+            mv "$contig_file" "$dst"
+        done
+
+}
+
+
+# if $MODE argument is provided, run the corresponding function
+if [[ "$MODE" == "reference" ]]; then
+    echo "Running reference-based transrate..."
+    run_reference_transrate "$QUERY" "$MANIFEST_FILE"
+elif [[ "$MODE" == "reads" ]]; then
+    echo "Running read-based transrate..."
+    run_read_transrate "$QUERY" "$MANIFEST_FILE"
+elif [[ "$MODE" == "both" ]]; then
+    echo "Running both reference and read-based transrate..."
+    run_longer_transrate "$QUERY" "$MANIFEST_FILE"
 else
-    echo "Using "$TARGET" and "$QUERY" and $MANIFEST_FILE to calculate the full-length transcript coverage"
-    # 
-    run_longer_transrate "$TARGET" "$QUERY" "$MANIFEST_FILE"
+    echo "No mode specified or invalid mode. Running default transrate..."
+        run_longer_transrate "$QUERY" "$MANIFEST_FILE"
 fi
 
-# rm -rf transrate_tmp_dir
+
+
+rm -rf transrate_tmp_dir
 
 exit
