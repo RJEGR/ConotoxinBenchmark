3dbb3c02ecbb45922126077740dd2d5b1ef47307
Ricardo Gore <rjhgore@gmail.com>
2025-09-10 11:44:02 -0700

    Update Run_mergepipe.sh

--- run_assemblers_dir/Run_mergepipe.sh
+++ run_assemblers_dir/Run_mergepipe.sh
@@ -7,34 +7,88 @@ CPU=$4
 MEM=$5
 FINAL_DIR=$6
 
+EXPORT=$PWD/run_assemblers_dir
+export PATH=$PATH:$EXPORT
 
+Run_plass.sh "$forward_fq" "$reverse_fq" "$OUTDIR" "$CPU" "$MEM" "$FINAL_DIR" &> /dev/null
 
-module load trinityrnaseq-v2.15.1
-
-which Trinity
-
-#call="Trinity --seqType fq --max_memory 100G --left $forward_fq --right $reverse_fq --no_normalize_reads --CPU $CPU --output trinity_out_dir.${OUTDIR} --full_cleanup"
-
-output_dir=${OUTDIR}/Trinity_out_dir # Trinity_out_dir prefix is mandatory for Trinity
-
-call="Trinity --seqType fq --max_memory 100G --left $forward_fq --right $reverse_fq --no_normalize_reads --CPU 24 --output $output_dir --full_cleanup"
-   
-
-echo "Executing: $call"
-
-eval $call
-
-f1=$(find "${OUTDIR}" -maxdepth 1 -type f -name 'Trinity_out_dir.Trinity.fasta')
+f1=$(find "${FINAL_DIR}" -maxdepth 1 -type f -name '*_MERGEPIPE.fa')
 
 echo "Results of the assembly found at: $f1"
- 
-final_fasta=$FINAL_DIR/${OUTDIR%_dir}.fa
 
-movecall="mv $f1 $final_fasta"
+fasta_tmp_1=$FINAL_DIR/assembly_1.fa
+
+movecall="mv $f1 $fasta_tmp_1"
 
 echo $movecall
 eval $movecall
 
-#rm *.fasta.gene_trans_map
+rm -rf $OUTDIR/*
+
+Run_spades.sh "$forward_fq" "$reverse_fq" "$OUTDIR" "$CPU" "$MEM" "$FINAL_DIR" &> /dev/null
+
+f1=$(find "${FINAL_DIR}" -maxdepth 1 -type f -name '*_MERGEPIPE.fa')
+
+echo "Results of the assembly found at: $f1"
+
+fasta_tmp_2=$FINAL_DIR/assembly_2.fa
+
+movecall="mv $f1 $fasta_tmp_2"
+
+echo $movecall
+eval $movecall
+
+rm -rf $OUTDIR/*
+
+Run_trinity.sh "$forward_fq" "$reverse_fq" "$OUTDIR" "$CPU" "$MEM" "$FINAL_DIR" &> /dev/null
+
+f1=$(find "${FINAL_DIR}" -maxdepth 1 -type f -name '*_MERGEPIPE.fa')
+
+echo "Results of the assembly found at: $f1"
+
+fasta_tmp_3=$FINAL_DIR/assembly_3.fa
+
+movecall="mv $f1 $fasta_tmp_3"
+
+echo $movecall
+eval $movecall
+
+#rm -rf $OUTDIR/*
+
+# Concatenate all assemblies into a single file
+
+fasta_file=$FINAL_DIR/${OUTDIR%_dir}.tmp.fa
+
+echo "Concatenating all assemblies into a single file... $fasta_file"
+
+cat $fasta_tmp_1 $fasta_tmp_2 $fasta_tmp_3 > $fasta_file
+
+thread_count=20
+
+INPUT=$fasta_file
+
+ID=0.9
+
+OUTFILE=$FINAL_DIR/${OUTDIR%_dir}.fa
+
+echo "Reduce redundancy filtering sequences with identical length and 100% length overlap in $OUTFILE"
+
+
+#OUTFILE=$FINAL_DIR/${INPUT%.*}.${ID}.mmseq.fasta
+
+
+EXPORT=/LUSTRE/bioinformatica_data/genomica_funcional/rgomez/Software/mmseqs/bin
+export PATH=$PATH:$EXPORT
+
+mmseqs createdb $INPUT $OUTDIR/sequenceDB 
+
+mmseqs clusthash $OUTDIR/sequenceDB  $OUTDIR/clusthashDB --threads $thread_count --min-seq-id $ID
+mmseqs clust $OUTDIR/sequenceDB  $OUTDIR/clusthashDB $OUTDIR/clusterDB --threads $thread_count
+
+mmseqs createsubdb $OUTDIR/clusterDB $OUTDIR/sequenceDB $OUTDIR/createsubDB
+mmseqs convert2fasta $OUTDIR/createsubDB $OUTFILE
+
+rm $fasta_tmp_1 $fasta_tmp_2 $fasta_tmp_3 $fasta_file
+
 
 exit
