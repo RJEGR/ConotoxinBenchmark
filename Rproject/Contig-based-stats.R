87506ff55eb26c9b39871f6cdefcf17e19cfccf5
Ricardo Gore <rgomez@cicese.edu.mx>
2026-04-07 12:54:31 -0700

    Update Contig-based-stats.R

--- Rproject/Contig-based-stats.R
+++ Rproject/Contig-based-stats.R
@@ -39,15 +39,15 @@ read_transrate_scores <- function(file_list) {
   )
   
   cat("\nReading\n")
-  cat(str_remove(file_list, paste0("^", dir, "/")))
+  cat(stringr::str_remove(file_list, paste0("^", dir, "/")))
   cat("\n")
   
   read_csv(file_list) %>%
-    mutate(rel_path = str_remove(file_list, paste0("^", dirname(dir), "/"))) |>
+    mutate(rel_path = stringr::str_remove(file_list, paste0("^", dirname(dir), "/"))) |>
     select(any_of(which_cols))
 }
 
-dir <- "//wsl.localhost/Debian/home/ricardo/transrate_contigs_dir/transrate_dir/"
+dir <- "/Users/rjegr/Documents/Windows/Debian/transrate_contigs_dir/transrate_dir/"
 
 str(file_list <- list.files(path = dir, pattern = "contigs.csv", recursive = T, full.names = TRUE))
 
@@ -62,9 +62,9 @@ DB1 <- transratedf |>
   # mutate(vfold_set = sapply(strsplit(rel_path, "_"), `[`, 1)) %>%
   # mutate(Assembler = sapply(strsplit(rel_path, "_"), `[`, 5)) |>
   dplyr::rename("protein_id" = contig_name, "Method" = rel_path) |>
-  select(protein_id, Method, p_bases_covered, p_good, score)
+  select(protein_id, Method, p_bases_covered, p_good, score, linguistic_complexity_6)
 
-dir <- "C://Users//cinai/OneDrive/Documentos/GitHub/ConotoxinBenchmark/INPUTS/BLAST_based_annotation_dir/"
+dir <- "/Users/rjegr/Documents/Windows/Documents/ConotoxinBenchmark/INPUTS/BLAST_based_annotation_dir/"
 
 f <- list.files(dir, full.names = T, pattern = ".rds")
 
@@ -80,3 +80,90 @@ DB1 |> ggplot(aes(p_good)) +
   geom_histogram() + 
   ggforce::facet_col(~ final_annotation) 
 
+# LOAD conoSorter protein results
+# SCOPE: Identify if causal relation between contig score and conosorter annotation
+
+# Assemblies2ConoSorterViz.R
+
+creates_domains <- function() {
+  
+  library(stringr)
+  
+  base_domains <- c("Mature", "Pro-region", "Signal")
+  
+  # Generate all possible combinations of length 1, 2, and 3
+  all_combos <- c()
+  
+  for (k in 1:length(base_domains)) {
+    combos <- combn(base_domains, k, simplify = FALSE)
+    formatted <- sapply(combos, function(x) {
+      paste0("(", paste(x, collapse = ")_("), ")")
+    })
+    all_combos <- c(all_combos, formatted)
+  }
+  
+  domains <- all_combos
+  print(domains)
+}
+
+names_to <-  c(creates_domains()[creates_domains() %in% "(Mature)_(Pro-region)_(Signal)"],
+               creates_domains()[!creates_domains() %in% "(Mature)_(Pro-region)_(Signal)"])
+
+recode_to <-structure(
+  c("Primary precursor", rep("Incomplete precursor", 6)),
+  names = names_to
+)
+
+dir <- "/Users/rjegr/Documents/Windows/Documents/ConotoxinBenchmark/INPUTS/ConoSorter_dir/"
+
+outName <- "protein_1" 
+
+f <- file.path(dir, paste0(outName, "_ConoSorter.rds"))
+
+DB2 <- read_rds(f) |> mutate(Method = gsub(".fa.transdecoder", "", Method)) 
+
+
+DB1 |> distinct(Method, protein_id)
+DB2 |> distinct(Method, protein_id)
+
+DB <- DB2 |> ungroup() |> 
+  mutate(protein_id = gsub(".p[0-9]+$", "", protein_id)) |>
+  drop_na(Region) |>
+  dplyr::mutate(Region = dplyr::recode_factor(Region, !!!recode_to)) |>
+  # distinct(Method, protein_id, Region, tab)  |> 
+  # Use right Join to count the number of full contigs
+  right_join(DB1, by = c("protein_id", "Method")) 
+
+DB |> 
+  # drop_na() |>
+  ggplot(aes(p_good, fill =Region)) + 
+  geom_histogram() + 
+  ggforce::facet_col(~ final_annotation, scales = "free_y") 
+
+DB |>
+  count(Region, final_annotation)
+
+
+DB |>
+  ggplot(aes(y = Superfamily, x = linguistic_complexity_6, fill = after_stat(x))) +
+  # geom_violin() +
+  # facet_grid(~ Superfamily) +
+  ggridges::geom_density_ridges_gradient(
+    jittered_points = F,
+    position = ggridges::position_points_jitter(width = 0.05, height = 0),
+    point_shape = '|', point_size = 3, point_alpha = 1, alpha = 1) +
+  scale_fill_viridis_c(option = "C") +
+  labs(y = "", x = "Reference coverage (TransRate)") +
+  theme_bw(base_family = "GillSans", base_size = 12) + 
+  theme(legend.position = "none", 
+        # axis.text.y = element_blank(), 
+        # axis.ticks.y = element_blank(), 
+        axis.title.x = element_text(size = 7)) +
+  scale_x_continuous(position = "top")
+
+DB |>
+  mutate(Method = sapply(strsplit(Method, "_"), `[`, 5)) |>
+  drop_na(final_annotation, Region) |>
+  ggplot(aes(linguistic_complexity_6, p_good, color = final_annotation)) +
+  geom_point() +
+  facet_grid(Method ~ final_annotation)
